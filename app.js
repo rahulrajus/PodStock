903d034c6e6e5cb0e9b591ed512c25dd0522844a
Rahul Rajan <rahul.rajanus@gmail.com>
2017-03-25 19:21:10 -0400

    server

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
